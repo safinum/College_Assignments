832714510c2d5e9fa6b289cd8b9cb2fcb047e8d2
Kamila Górniak <114913285+safinum@users.noreply.github.com>
2022-10-04 16:57:04 +0200

    Delete zadanie3.1 directory

--- zadanie3.1/Figura.java
+++
@@ -1,6 +0,0 @@
-public abstract class Figura implements Obliczenia {
-
-    public void pokazInfo() {
-        System.out.printf("%s: obwod = %.2f; pole = %.2f\n", this.getClass().getName(), obliczObwod(), obliczPole());
-    }
-}
